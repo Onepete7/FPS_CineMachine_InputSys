f0023e7144660262884d7036c8385ffb6e2d6214
Onepete7 <114758771+Onepete7@users.noreply.github.com>
2023-08-31 11:18:02 +0200

    Initial commit to main

---
+++ CMFeatInputSystem/Assets/1_MyStuff/FPS_Movement/Scripts/OP7_PlayerMovement.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class OP7_PlayerMovement : MonoBehaviour
+{
+
+    [Header("Movement")]
+
+    public float maxMoveSpeed;
+
+    public Transform orientation;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    [Header("UI")]
+    public TextMeshProUGUI currentSpeedText;
+
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.freezeRotation = true;
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        //handle Drag
+        if (grounded)
+            rigidBody.drag = groundDrag;
+        else
+            rigidBody.drag = 0.0f;
+
+        if (currentSpeedText != null)
+        {
+            currentSpeedText.text = "Speed: " + rigidBody.velocity.magnitude.ToString("F2");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+
+
+    private void MovePlayer()
+    {
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        //on ground
+        if (grounded)
+            rigidBody.AddForce(moveDirection.normalized * maxMoveSpeed * 10.0f, ForceMode.Force);
+
+        //in air 
+        else if (!grounded)
+            rigidBody.AddForce(moveDirection.normalized * maxMoveSpeed * 10.0f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rigidBody.velocity.x, 0.0f, rigidBody.velocity.z);
+
+        // limit velocity if needed
+
+        if (flatVel.magnitude > maxMoveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * maxMoveSpeed;
+            rigidBody.velocity = new Vector3(limitedVel.x, rigidBody.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0.0f, rigidBody.velocity.z);
+
+        rigidBody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+}
